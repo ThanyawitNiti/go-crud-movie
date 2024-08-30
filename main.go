6dc955c3b4099eb87f36bd9b3625e20df4bd1d9a
Palm-Thanyawit <thanyawit.nitipornwattana@gmail.com>
2024-08-31 01:18:20 +0700

    create : struct and method

--- main.go
+++ main.go
@@ -2,8 +2,36 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
+
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+var movies []Movie
 
 func main() {
 	fmt.Printf("Hello world!")
+	r := mux.NewRouter()
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovies).Methods("GET")
+	r.HandleFunc("/movies", createMovies).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovies).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovies).Methods("DELETE")
+
+	fmt.Printf("Starting server at port 9000\n")
+	log.Fatal(http.ListenAndServe(":9000", r))
 }
